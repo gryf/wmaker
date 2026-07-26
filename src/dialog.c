17d44e15033ada29b0100954e6fa8fd7fe76b220
Andreas Metzler <ametzler@bebt.de>
2026-07-26 14:14:32 +0200

    Update GPL grant in info panel.
    Avoid outdated FSF postal address.

--- src/dialog.c
+++ src/dialog.c
@@ -1587,9 +1587,8 @@ void wShowLegalPanel(WScreen *scr)
 			 "of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. "
 			 "See the GNU General Public License for more details.\n\n"
 			 "    You should have received a copy of the GNU General Public "
-			 "License along with this program; if not, write to the Free Software "
-			 "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA"
-			 "02110-1301 USA."));
+			 "License along with this program; if not, see "
+			 "<https://www.gnu.org/licenses/>."));
 
 	WMRealizeWidget(panel->win);
 	WMMapSubwidgets(panel->win);
